631f48533114b96ef0927c38a3a9ad62b4185539
annasz03 <63146985+annasz03@users.noreply.github.com>
2024-09-02 21:54:24 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,7 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAeCz85zIUe32oHVAsDQiZqudn1dhdKatE",
-  authDomain: "artsy-b4a41.firebaseapp.com",
-  projectId: "artsy-b4a41",
-  storageBucket: "artsy-b4a41.appspot.com",
-  messagingSenderId: "162521145916",
-  appId: "1:162521145916:web:ea7743934a59101c315939",
-  measurementId: "G-GDQ509FGZG"
-};
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
